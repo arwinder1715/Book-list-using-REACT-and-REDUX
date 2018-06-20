1a821ae31ed39a5e013694778b83ce659f8afbc0
ARWINDER SINGH <arwinder1715@gmail.com>
2018-06-20 02:17:31 -0400

    Add files via upload

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+  return [
+    { title: "JavaScript: The good parts",pages:101 },
+    { title: "The Dark tower" ,pages:84},
+
+    { title: "Harry porter" ,pages:8},
+
+    { title: "Spiderman",pages:88 }
+  ];
+}
